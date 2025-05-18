494bcfaa2b13dd45ef19e63fe6a64205192abcce
hihassan1998 <hassanihussain1998@gmail.com>
2025-05-18 02:09:45 +0200

    Add docker-composer for settingup mysql env in docker

--- src/db_credentials.php
+++ src/db_credentials.php
@@ -1,5 +1,13 @@
 <?php
-define("DB_SERVER", "172.26.32.1");
-define("DB_USER", "dbadm");
-define("DB_PASS", "P@ssw0rd");
-define("DB_NAME", "d0019e_blog");
+// locally use these credentials
+// define("DB_SERVER", "172.26.32.1");
+// define("DB_USER", "dbadm");
+// define("DB_PASS", "P@ssw0rd");
+// define("DB_NAME", "d0019e_blog");
+
+
+// remote use and local use credentials
+define('DB_SERVER', getenv('DB_SERVER') ?: '172.26.32.1');
+define('DB_USER', getenv('DB_USER') ?: 'dbadm');
+define('DB_PASS', getenv('DB_PASS') ?: 'P@ssw0rd');
+define('DB_NAME', getenv('DB_NAME') ?: 'd0019e_blog');
